e8c90096952ee782ddd7d8364348bf276001fc94
Anuja <anujajadhav0704@gmail.com>
2025-01-22 12:49:56 +0530

    spring security configuration_part 1

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.scm.config;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -13,18 +15,23 @@ import org.springframework.security.provisioning.UserDetailsManager;
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     //user create and login page, using java code using in-memory service
     @Bean
     public UserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder){
 
-        UserDetails user1 = User.withUsername("admin")
+        UserDetails admin = User.withUsername("admin")
                 .roles("ADMIN","USER")
                 .password("admin").build();
 
-        UserDetails user2 = User.withUsername("user123")
+        UserDetails user1 = User.withUsername("user123")
                 .password("password").build();
 
-        return new InMemoryUserDetailsManager();
+        return new InMemoryUserDetailsManager(admin,user1);
     }
 
 }
