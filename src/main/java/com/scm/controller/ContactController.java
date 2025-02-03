51f9c330ef6b2903bfa67deac37002cbdb0ccecc
Anuja <anujajadhav0704@gmail.com>
2025-02-03 21:19:50 +0530

    'Add Contact' form validations

--- src/main/java/com/scm/controller/ContactController.java
+++ src/main/java/com/scm/controller/ContactController.java
@@ -4,12 +4,19 @@ import com.scm.entities.Contact;
 import com.scm.entities.User;
 import com.scm.forms.ContactForm;
 import com.scm.helper.Helper;
+import com.scm.helper.Message;
+import com.scm.helper.MessageType;
 import com.scm.services.ContactService;
 import com.scm.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,7 +43,17 @@ public class ContactController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String saveContact(Model model, ContactForm contactForm, Authentication authentication){
+    public String saveContact(@Valid @ModelAttribute ContactForm contactForm, BindingResult bindingResult,
+                              Authentication authentication, HttpSession httpSession){
+
+        //validate form
+        if(bindingResult.hasErrors()){
+            httpSession.setAttribute("message", Message.builder()
+                    .content("Please resolve the following errors!")
+                    .type(MessageType.red)
+                    .build());
+            return "user/add_contact";
+        }
 
         String username = Helper.getEmailOfLoggedInUser(authentication);
 
@@ -55,8 +72,14 @@ public class ContactController {
         contact.setUser(user);
 
         contactService.save(contact);
-
         System.out.println(contactForm);
+
+        //message to be displayed on view
+        httpSession.setAttribute("message", Message.builder()
+                .content("Contact added successfully!")
+                .type(MessageType.green)
+                .build());
+
         return "redirect:/user/contact/add";
     }
 
